575f9c817288932d27e6f43a0f646d0362966f7f
AidounNejma <guermachenejma@gmail.com>
2021-12-24 18:48:00 +0100

    test js
    ne fonctionne pas (j'ai fait n'importe quoi)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Quizz HTML/CSS/JAVASCRIPT
 
+
 ## Allumer le serveur pour la "database" json
 ```bash 
 npm package.json
@@ -8,3 +9,25 @@ OU
 ```bash 
 json-server --watch package.json
 ```
+
+
+## Télécharger l'extension Live Server
+- Sur Visual Studio Code, l'extension est au nom de Ritwick Dey
+- Une fois installée, pour démarrer le serveur sur le fichier HTML appuyez simultanément sur:
+    - Windows: pour ouvrir le serveur
+        ```bash 
+        alt+L alt+O 
+        ```
+        Windows: pour fermer le serveur
+        ```bash 
+        alt+L, alt+C
+        ```
+
+    - Windows: pour ouvrir le serveur
+        ```bash 
+        cmd+L cmd+O 
+        ```
+        Windows: pour fermer le serveur
+        ```bash 
+        cmd+L, cmd+C
+        ```
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,48 @@
+/* Création de variable / récupération des élements de ma page HTML */
+let img = document.getElementById('img');
+//console.log(img);
+
+let question = document.getElementById('question');
+//console.log(question)
+
+let suggestionsLabel = document.getElementsByClassName('suggestionsLabel');
+//console.log(suggestionsLabel.item(0));
+
+let suggestionsInput = document.getElementsByClassName('suggestionsInput');
+console.log(suggestionsInput)
+
+let number = document.getElementById('number');
+//console.log(number)
+
+let next = document.getElementById('next');
+//console.log(next)
+
+/* Appel du fichier JSON */
 fetch('package.json')
                 .then(res => res.json())
                 .then(data => {
-                    console.log(data);
+                    console.log(data.HTML)
+                    next.addEventListener('click', changeQuestion(data.HTML));
                 })
                 .catch(err => console.error(err));
+
+
+
+
+/* Fonction de generation des question */
+function changeQuestion(data){
+    for(let i = 0; i < 10; i++){
+        for(let m = 0; m < 10; m++){
+            number.innerHTML = data[m].id;
+            question.innerHTML = data[m].question;
+            img.src = data[m].image;
+        }
+        
+        for(let n = 0; n < 10; n++){
+            suggestionsLabel.item(i).innerHTML = data[n].propositions[i];
+            suggestionsInput[i].value = data[n].propositions[n];
+        }
+    }
+    
+}
+
