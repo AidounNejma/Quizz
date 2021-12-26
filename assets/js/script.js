056aa3f4090846f22c7b0415fd82fc65bf4fc76c
AidounNejma <guermachenejma@gmail.com>
2021-12-26 02:03:34 +0100

    Modification du js et de l'html

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,14 @@ let suggestionsInput = document.getElementsByClassName('suggestionsInput');
 let number = document.getElementById('number');
 //console.log(number)
 
+let number2 = document.getElementById('number2');
+//console.log(number2)
+
 let next = document.getElementById('next');
 //console.log(next)
+
+let start = document.getElementById('start');
+//console.log(start)
 
 let title = document.getElementsByClassName('pageTitleChange');
 //console.log(title[0])
@@ -31,7 +37,6 @@ let submit = document.getElementById('submit');
 /* Variables qui vont nous être utiles dans les fonctions */
 var index = 0; /* Pour se positionner dans l'index de mon array */
 let score = 0; /* Pour incrémenter le score */
-let numberIncrement = 0;  // Numéro des questions 
 var questionArray = []; // Tableau vide pour copier ma data dedans
 
 /* --------------------------------------------------------------- */
@@ -48,7 +53,7 @@ lastPieceUrl = currentUrl.substring(currentUrl.lastIndexOf("/") + 1); // on stoc
 
 /* A l'arrivée sur la page */
 
-function firstPage(){
+function firstPage(){ //Cette fonction va donner un visuel au premier chargement de la page
     if(lastPieceUrl == "quizz.html?random"){
         title[0].innerHTML = "Aléatoire";
     }
@@ -63,21 +68,32 @@ function firstPage(){
     }
 
     for(let i = 0; i < suggestionsInput.length; i++){
-        suggestionsInput[i].style.display = 'none'
+        suggestionsInput[i].style.display = 'none';
     }
+
+    next.style.display = "none";
 
     submit.style.display = "none";
 }
+window.onload = firstPage(); // j'appelle ma fonction au chargement de la page 
 
-function eraseFirstPage(){
+/* --------------------------------------------------------------- */
+
+/* Fonction pour supprimer la première page */
+
+function eraseFirstPage(){ //Cette fonction va enlever le visuel de la premier page (fonction au dessus) au clic de "commencer le test"
     for(let i = 0; i < suggestionsInput.length; i++){
         suggestionsInput[i].style.display = 'inline';
     }
-    next.innerHTML = "Suivant";
+    
     paragraphStart.style.display = 'none';
+    next.style.display = "inline";
+    start.style.display = "none";
+    displayQuizz()
 }
 
-window.onload = firstPage();
+start.addEventListener('click', eraseFirstPage);
+
 
 /* --------------------------------------------------------------- */
 
@@ -95,31 +111,32 @@ const getData = async() => { //Fonction asyncrone pour récupérer les données 
     const response = await fetch(url); // fetch de mon url
     const data = await response.json(); // je récupère la reponse de ma promesse et je la mets au format JSONs
     
-    if (lastPieceUrl == "quizz.html?random") { //conditions pour récupérer les datas qui nous intéresse selon le clic de la page précédente
+    //conditions pour récupérer les datas qui nous intéresse selon le clic de la page précédente
+    if (lastPieceUrl == "quizz.html?random") { //On récupère toute la data (ne fonctionne pas encore)
         //console.log(data);
-        shuffleArray(data);
-        return data;
+        shuffleArray(data); //je shuffle mon array en faisant appel à la fonction que j'ai créé au dessus
+        return data; // je retourne ma data
     }
     
-    if (lastPieceUrl == "quizz.html?html") {
+    if (lastPieceUrl == "quizz.html?html") {// On récupère le HTML
         //console.log(data.HTML)
         shuffleArray(data.HTML);
         return data.HTML;
     }
     
-    if (lastPieceUrl == "quizz.html?css") {
+    if (lastPieceUrl == "quizz.html?css") { // On récupère le CSS
         //console.log(data.CSS)
         shuffleArray(data.CSS);
         return data.CSS;
     }
 
-    if (lastPieceUrl == "quizz.html?javascript") {
+    if (lastPieceUrl == "quizz.html?javascript") { // On récupère le Javascript
         //console.log(data.Javascript)
         shuffleArray(data.Javascript);
         return data.Javascript;
     }
 }
-window.onload = getData();
+window.onload = getData(); // j'appelle ma fonction au chargement de la page 
 /* --------------------------------------------------------------- */
 
 /* Copie de l'array */
@@ -127,65 +144,97 @@ window.onload = getData();
 const copyArray = async(data)=>{
     const myData = await getData(data);
     //console.log(myData)
-    questionArray.push(...myData);
-    return questionArray;
-
+    questionArray.push(...myData); //Je stocke dans mon array vide (questionArray) => myData qui contient mes données du fichier JSON
+    return questionArray; //Je retourne questionArray
     //console.log(questionArray);
 }
 
 window.onload = copyArray(); // j'appelle ma fonction au chargement de la page 
-console.log(questionArray);
+//console.log(questionArray);
 
 /* --------------------------------------------------------------- */
 
 /* Fonction pour afficher tout ça */
 function displayQuizz(){
-    displayQuestion();
-}
-
-/* --------------------------------------------------------------- */
-
-/* Fonction qui va afficher les questions */
-function displayQuestion(){
-    console.log(questionArray);
-    number.innerHTML = index;
-    if(index < questionArray.length){
-        index++;
-        question.innerHTML = questionArray[index].question;
-        img.src = questionArray[index].image;
-        for(let n = 0; n < 4; n++){
-        suggestionsLabel[n].innerHTML = questionArray[index].propositions[n];
-        suggestionsLabel[n].setAttribute("for", questionArray[index].propositions[n]);
-        suggestionsInput[n].setAttribute("value", questionArray[index].propositions[n]);
-        suggestionsInput[n].setAttribute("id", questionArray[index].propositions[n]);
-        }
-    }
     
+    if(index <= questionArray.length){ /* Si l'index est inf ou égal à la longueur de mon array alors */
+        displayQuestion();
+        //storeAnswers()
+        //recordScore()
+    }
+    if(index > questionArray.length){ /* Si l'index est sup à la longueur de mon array alors */
+        //finalSubmit()
+    }
+
 }
 
 next.addEventListener('click', displayQuizz);
 
 /* --------------------------------------------------------------- */
 
+/* Fonction qui va afficher les questions */
+number.innerHTML = index;
 
-/* Fonction pour enregistrer les données dans le local Storage du navigateur */
+function displayQuestion(){
+    console.log(questionArray);
+    /* Incrémenter le nombre de questions */
+    index++;
+    number.innerHTML = index;
+    number2.innerHTML = questionArray.length;
 
-/* function storeAnswers(i){
-    if(localStorage.getItem('answers') == null){
+    /* Affectation des valeurs de l'array à mes variables HTML */
+    question.innerHTML = questionArray[index].question;
+    img.src = questionArray[index].image;
+
+    for(let n = 0; n < 4; n++){
+        suggestionsLabel[n].innerHTML = questionArray[index].propositions[n];
+        suggestionsLabel[n].setAttribute("for", questionArray[index].propositions[n]);
+        suggestionsInput[n].setAttribute("value", questionArray[index].propositions[n]);
+        suggestionsInput[n].setAttribute("id", questionArray[index].propositions[n]);
+    }
+}
+
+/* --------------------------------------------------------------- */
+
+/* Pour incrémenter le score à chaque bonne réponse */
+function recordScore(){
+    
+    if(questionArray[index].réponse == suggestionsInput[index].checked.value){ // Si la réponse stockée dans mon questionArray (la bonne réponse) est égale à la valeur de l'input radio qui est coché alors:
+        score++; // on incrémente de 1
+    } else { //sinon
+        score + 0; // on ajoute 0
+    } 
+}
+
+/* --------------------------------------------------------------- */
+
+/* Fonction pour enregistrer les réponses dans le local Storage du navigateur */
+
+function storeAnswers(){
+    
+    if(localStorage.getItem('answers') == null && localStorage.getItem('score') == null){
         localStorage.setItem('answers', '[]');
+        localStorage.setItem('score', '[]');
     }
 
-    let new_value = suggestionsInput[i].value;
+    let new_value = suggestionsInput[index].checked.value;
     let old_value = JSON.parse(localStorage.getItem('answers'));
     old_value.push(new_value);
 
     localStorage.setItem('answers', JSON.stringify(old_value));
+
 }
 
-    if(data[i].réponse == suggestionsInput[i].value){
-    
-    } else {
-
-    } 
- */
 /* --------------------------------------------------------------- */
+
+/* Fonction qui s'enclenche lors du dernier submit*/
+
+function finalSubmit(){
+    
+    /* Permet d'enregistrer le score du joueur dans le local storage */
+    let newScore = score;
+    let oldScore = JSON.parse(localStorage.getItem('score'));
+    oldScore.push(newScore);
+
+    localStorage.setItem('score', JSON.stringify(oldScore));
+}
